54754fcf2af2494e9f96f4426139fc829fbf5926
rjb255 <62777572+rjb255@users.noreply.github.com>
2020-03-28 11:54:32 +0000

    Couple of edits
    Added a good index to the table as this will allow transactions to just be appended to the end.
    Added an actual timestamp, although there probably is a better way of doing it using numpy.

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import numpy as np
 import pandas as pd
-
+from datetime import datetime as dtt
 def shopDefine():
   num_goods = 100
   num_locations = 10
 
   shopStock = pd.DataFrame({
+    "Good #": np.arange(num_goods),
     "Location": np.random.randint(num_locations, size = num_goods),
     "Quantity": np.random.randint(20, 100, num_goods),
     "Timestamp": np.zeros(num_goods),
     "Reference": np.full(num_goods, 2), # 0 = purchase, 1 = partial restock, 2 = full shop restock
-    "Timestamp": np.zeros(num_goods)
+    "Timestamp": [dtt.now().strftime('%H:%M %d-%m-%Y ') for i in range(num_goods)]
   })
 
   total = shopStock.groupby(["Location"]).apply(lambda x : sum(x["Quantity"])).values
